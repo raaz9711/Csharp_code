6492e0df009d11d856c68284bf0b9926f19ca927
raj kumar <raaz.kumar015@gmail.com>
2023-08-22 09:14:42 +0530

    array foreach loop

---
+++ array.cs
@@ -0,0 +1,24 @@
+        //method 1
+        int[] arr = new int[5];
+        // method 2
+
+        int[] arr1 = { 1, 2, 3, 4, 5, 9, 10 };
+
+        //method 3
+        int[] arr2 = new int[] { 100, 2, 3, 400, 5, 9, 10 };
+
+        //foreach (int i in arr2)
+        //{
+        //    Console.WriteLine("The Value in arr {0}",i);
+
+        //}
+
+        //Console.ReadKey();
+
+
+        //string[] freindList = new string[] { "Michael", "Danny", "Love" };
+
+        //    foreach (var item in freindList)
+        //    {
+        //        Console.WriteLine("Hello {0} my friend",item);
+        //    }
